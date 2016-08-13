5d28b9131708a417246307c32baadc5893438e2b
jorgeluis11 <perez.jorge718@gmail.com>
2016-08-13 11:46:30 -0400

    models and some views created

--- models/jobs.js
+++ models/jobs.js
@@ -1,11 +1,11 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var Jobs = new Schema({
+let Jobs = new Schema({
   job_title: { type: String, required: true, maxlength: 55},
   job_type: { type: String, required: true, allowedValues: ['Full-time', 'Part-time', "Contract", "Freelance"] },
   job_description: { type: String, required: true, maxlength: 255 },
-  location_type: { type: String, required: true, allowedValues: ['remote', 'OnSite'] },
+  location_type: { type: String, required: true, allowedValues: ['Remote', 'OnSite'] },
   company_name: { type: String, required: true, maxlength: 55},
   company_url: String,
   company_location: { type: String, required: true, maxlength: 55 },
@@ -18,3 +18,5 @@ var Jobs = new Schema({
   created_at: Date,
   updated_at: Date
 });
+
+module.exports = Jobs;
--- routes/jobs.js
+++ routes/jobs.js
@@ -1,13 +1,40 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const mongoose = require("mongoose");
+const Jobs = require('../models/jobs');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('test');
+  let job = mongoose.model('Jobs', Jobs);
+    job.find({}, function(err, jobs) {
+      res.json(jobs);
+    })
+  
 });
 
-router.get('/:id', function(req, res, next) {
-  res.send('id');
+router.get('/view/:id', function(req, res, next) {
+  var id = req.params.id;
+  res.send(`Post ${id}`);
+});
+
+router.get('/insert', function(req, res, next) {
+  let job = mongoose.model('Jobs', Jobs);
+
+  job({
+    job_title: "Job Title",
+    job_type: "Full-time",
+    job_description: 'Job Description',
+    location_type: "Remote" ,
+    company_name: "Company Name",
+    company_url: "www.google.com",
+    company_location: "San Juan",
+    company_description: 'Company Description',
+    company_apply: 'Job Apply description',
+    created_at: new Date(),
+    updated_at: new Date()
+  }).save();
+  res.send('sample insert!');
+
 });
 
 
