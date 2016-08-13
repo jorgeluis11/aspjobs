c02ac5537668a15de9a68004b2508c726ab999d3
Jose Ivan Marquez <ivan.marquez.gdev@gmail.com>
2016-08-12 20:50:19 -0400

    Changing to ES-6
    Adding ES6 syntax to improve code readability.

--- app.js
+++ app.js
@@ -1,60 +1,75 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+const routes = require('./routes/index');
+const users = require('./routes/users');
 
-var app = express();
-
-// view engine setup
+let app = express();
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+class Server {
 
-app.use('/', routes);
-app.use('/users', users);
+    constructor() {
+        this.config();
+        this.routeConfig();
+        this.errorConfig();
+    }
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+    config() {
+        app.use(logger('dev'));
+        app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded({extended: false}));
+        app.use(cookieParser());
+        app.use(require('node-sass-middleware')({
+            src: path.join(__dirname, 'public'),
+            dest: path.join(__dirname, 'public'),
+            indentedSyntax: true,
+            sourceMap: true
+        }));
+        app.use(express.static(path.join(__dirname, 'public')));
+    }
 
-// error handlers
+    routeConfig() {
+        app.use('/', routes);
+        app.use('/users', users);
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+        app.use((req, res, next) => {
+            let err = new Error('Not Found');
+            err.status = 404;
+            next(err);
+        });
+    }
+
+    errorConfig() {
+        if (app.get('env') === 'development') {
+            app.use((err, req, res, next) => {
+                res.status(err.status || 500);
+                res.render('error', {
+                    message: err.message,
+                    error: err
+                });
+            });
+        }
+
+        app.use((err, req, res, next) => {
+            res.status(err.status || 500);
+            res.render('error', {
+                message: err.message,
+                error: {}
+            });
+        });
+    }
+
+    static bootstrap() {
+        return new Server();
+    }
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
+Server.bootstrap();
 
 module.exports = app;
