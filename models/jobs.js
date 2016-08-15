86ba273a495e066c9f3dea3d607515b843277f5a
jorgeluis11 <perez.jorge718@gmail.com>
2016-08-15 18:51:34 -0400

    template layout addeded

---
+++ models/jobs.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+let jobsSchema = new Schema({
+  job_title: { type: String, required: true, maxlength: 55},
+  job_type: { type: String, required: true, allowedValues: ['Full-time', 'Part-time', "Contract", "Freelance"] },
+  job_description: { type: String, required: true, maxlength: 255 },
+  location_type: { type: String, required: true, allowedValues: ['Remote', 'OnSite'] },
+  company_name: { type: String, required: true, maxlength: 55},
+  company_url: String,
+  company_location: { type: String, required: true, maxlength: 55 },
+  company_description: { type: String, required: true, maxlength: 255 },
+  company_apply: { type: String, required: true, maxlength: 255 },
+//   meta: {
+//     age: Number,
+//     website: String
+//   },
+  created_at: Date,
+  updated_at: Date
+});
+
+let Jobs = mongoose.model('Jobs', jobsSchema);
+
+
+module.exports = Jobs;
---
+++ routes/jobs.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require("mongoose");
+// const Jobs = require('../models/jobs');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    // Jobs.find({}, function(err, jobs) {
+      // console.log(jobs);
+    res.render('jobs');
+    // })
+});
+
+router.get('/view/:id', function(req, res, next) {
+  var id = req.params.id;
+  res.send(`Post ${id}`);
+});
+
+router.get('/insert', function(req, res, next) {
+  // Jobs({
+  //   job_title: "Job Title",
+  //   job_type: "Full-time",
+  //   job_description: 'Job Description',
+  //   location_type: "Remote" ,
+  //   company_name: "Company Name",
+  //   company_url: "www.google.com",
+  //   company_location: "San Juan",
+  //   company_description: 'Company Description',
+  //   company_apply: 'Job Apply description',
+  //   created_at: new Date(),
+  //   updated_at: new Date()
+  // }).save();
+  res.render('jobs');
+
+});
+
+
+module.exports = router;
