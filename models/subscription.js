d78092f394031e8589de4487e8aa64dd919b1f70
jorgeluis11 <perez.jorge718@gmail.com>
2016-08-28 21:47:54 -0400

    subscribe working

--- models/subscription.js
+++ models/subscription.js
@@ -1,12 +1,57 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const Token = require('./token');
+const uuid = require('node-uuid');
+const helper = require('sendgrid').mail;
+// const Subscription = require('./models/subscription');
 
 let subscriptionSchema = new Schema({
-  email: { type: String, required: true, maxlength: 55},
+  email: { type: String, required: true, maxlength: 55, unique: true},
   schedule: { type: String, required: true, allowedValues: ['Daily', 'Weekly'] },
   created_at: {type: Date, required: true, default: Date.now},
   verified: Boolean,
   // token: {type: String, required: true},
 });
+
+subscriptionSchema.statics.createSubscription = (email) =>{
+      console.log("Create!");
+      var token = uuid.v4();
+
+      var subscription = subscriptionSchema({
+            email: email,
+            schedule: "Daily",
+            verified: false
+          });
+          subscription.save((error, subs) => {
+          Token({
+            _subscription:subs.id,
+            token
+          }).save();
+
+          let from_email = new helper.Email("hello@aspjobs.com");
+          let subject = `ASP Jobs: Please Confirm Subscription`;
+          let content = new helper.Content('text/html', "<a href='http://localhost:3000/subscription/confirm/"+ token +"'>Subscribe</a>");
+
+          let to_email = new helper.Email(email);
+          var sg = require('sendgrid')("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");
+          // var sg = require('sendgrid')("process.env.sendGridAPI");
+          let mail = new helper.Mail(from_email, subject, to_email, content)
+
+          var request = sg.emptyRequest({
+            method: 'POST',
+            path: '/v3/mail/send',
+            body: mail.toJSON()
+          });
+
+
+          sg.API(request, function(error, response) {
+            console.log(response.statusCode)
+            console.log(response.body)
+            console.log(response.headers)
+          });
+
+    });
+
+}
 
 module.exports = mongoose.model('Subscription', subscriptionSchema);
--- routes/subscription.js
+++ routes/subscription.js
@@ -1,67 +1,141 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require("mongoose");
-const uuid = require('node-uuid');
-const helper = require('sendgrid').mail;
-
 const Subscription = require('../models/subscription');
 const Token = require('../models/token');
+const uuid = require('node-uuid');
+const helper = require('sendgrid').mail;
 
 router.post('/insert', (req, res) => {
   console.log(req.body)
     if(!req.body.email && req.body.email.test(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i)){
       req.json({success:false});
     }
-
-    var token = uuid.v4();
     
-    var subscription = Subscription({
-      email: req.body.email,
-      schedule: "Daily",
-      verified: false
+    Subscription.findOne({email: req.body.email},function( err, sub){
+      if(!sub){
+         var token = uuid.v4();
+
+        var subscription = Subscription({
+              email: req.body.email,
+              schedule: "Daily",
+              verified: false
+            });
+          subscription.save((error, subs) => {
+          Token({
+            _subscription:subs.id,
+            token
+          }).save();
+
+          let from_email = new helper.Email("hello@aspjobs.com");
+          let subject = `ASP Jobs: Please Confirm Subscription`;
+          let content = new helper.Content('text/html', "<a href='http://localhost:3000/subscription/confirm/"+ token +"'>Subscribe</a>");
+
+          let to_email = new helper.Email(req.body.email);
+          var sg = require('sendgrid')("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");
+          // var sg = require('sendgrid')("process.env.sendGridAPI");
+          let mail = new helper.Mail(from_email, subject, to_email, content)
+
+          var request = sg.emptyRequest({
+            method: 'POST',
+            path: '/v3/mail/send',
+            body: mail.toJSON()
+          });
+
+
+          sg.API(request, function(error, response) {
+            console.log(response.statusCode)
+            console.log(response.body)
+            console.log(response.headers)
+          });
+          res.render("verify", {message:'Activate your subscription on your <a target="_blank" href="https://mail.google.com/mail/u/0/#search/aspjobs">emails address</a>'});
+        });
+
+      }else{
+
+        if(sub.active){
+          res.render("verify", {message:'This email address is already subscribed.'});
+        }else{
+          Token.findOne({_subscription: sub.id},function( err, t){
+            if(t){
+              console.log("token already created");
+              var token = uuid.v4();
+
+              let from_email = new helper.Email("hello@aspjobs.com");
+              let subject = `ASP Jobs: Please Confirm Subscription`;
+              let content = new helper.Content('text/html', "<a href='http://localhost:3000/subscription/confirm/"+ t.token +"'>Subscribe</a>");
+
+              let to_email = new helper.Email(req.body.email);
+              var sg = require('sendgrid')("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");
+              // var sg = require('sendgrid')("process.env.sendGridAPI");
+              let mail = new helper.Mail(from_email, subject, to_email, content)
+
+              var request = sg.emptyRequest({
+                method: 'POST',
+                path: '/v3/mail/send',
+                body: mail.toJSON()
+              });
+
+              sg.API(request, function(error, response) {
+                console.log(response.statusCode)
+                console.log(response.body)
+                console.log(response.headers)
+              });
+              res.render("verify", {message:'The email confirmation was sent, verify your subscription in your <a target="_blank" href="https://mail.google.com/mail/u/0/#search/aspjobs">email address</a>'});
+            }else{
+              console.log("token not created");
+              var token = uuid.v4();
+              
+              Token({
+                _subscription:sub.id,
+                token
+              }).save();
+
+              let from_email = new helper.Email("hello@aspjobs.com");
+              let subject = `ASP Jobs: Please Confirm Subscription`;
+              let content = new helper.Content('text/html', "<a href='http://localhost:3000/subscription/confirm/"+ token +"'>Subscribe</a>");
+
+              let to_email = new helper.Email(req.body.email);
+              var sg = require('sendgrid')("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");
+              // var sg = require('sendgrid')("process.env.sendGridAPI");
+              let mail = new helper.Mail(from_email, subject, to_email, content)
+
+              var request = sg.emptyRequest({
+                method: 'POST',
+                path: '/v3/mail/send',
+                body: mail.toJSON()
+              });
+
+              sg.API(request, function(error, response) {
+                console.log(response.statusCode)
+                console.log(response.body)
+                console.log(response.headers)
+              });
+              res.render("verify", {message:'The email confirmation was sent, verify your subscription in your <a target="_blank" href="https://mail.google.com/mail/u/0/#search/aspjobs">email address</a>'});
+            }
+          });
+
+        }  
+
+      }
     });
-    subscription.save((error, subs) => {
-    Token({
-      _subscription:subs.id,
-      token
-    }).save();
-
-    let from_email = new helper.Email("hello@aspjobs.com");
-    let subject = `ASP Jobs: Please Confirm Subscription`;
-    let content = new helper.Content('text/html', "<a href='http://localhost:3000/subscription/confirm/"+ token +"'>Subscribe</a>");
-
-    let to_email = new helper.Email(req.body.email);
-    var sg = require('sendgrid')("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");
-    // var sg = require('sendgrid')("process.env.sendGridAPI");
-    let mail = new helper.Mail(from_email, subject, to_email, content)
-
-    var request = sg.emptyRequest({
-      method: 'POST',
-      path: '/v3/mail/send',
-      body: mail.toJSON()
-    });
-
-
-    sg.API(request, function(error, response) {
-      console.log(response.statusCode)
-      console.log(response.body)
-      console.log(response.headers)
-    });
-
-    res.render("verify");
-    });
-
-    
-
 });
+
 
 router.get('/confirm/:token', (req, res) => {
-  Token.find({token:req.params.token}, (err, token)=>{
-    console.log(token);
-    res.send("confirmed!");
+  Token.findOne({token:req.params.token}, (err, token)=>{
+    if(!token){
+      // redirect("index")
+    }
+    console.log(token._subscription);
+    Subscription.findById(token._subscription, ( err, sub) => {
+      console.log("sub",sub);
+      sub.verified = true;
+      sub.save()
+      token.remove();
+      res.render("verify", {message:'Thanks for your subscribtion!'});
+    });
   });
- 
 });
-
 
 module.exports = router;
