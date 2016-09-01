d806f5ca0b6f353da1d4479bde70491829b632a6
Luis J. Perez <luisjorge129@gmail.com>
2016-08-31 23:59:00 -0400

    Changes to model.

--- models/jobs.js
+++ models/jobs.js
@@ -5,15 +5,12 @@ var slug = require('mongoose-slug');
 let jobsSchema = new Schema({
   textfield_27716413: { type: String, required: true, maxlength: 55}, // Job Title
   list_27717784_choice: { type: String, required: true, allowedValues: ['Full-time', 'Part-time', "Contract", "Freelance"] }, // Job Type
-  textarea_27716421: { type: String, required: true, maxlength: 255 }, // Job Description
+  textarea_27716421: { type: String, required: true }, // Job Description
   list_27718927_choice: { type: String, required: true, allowedValues: ['Remote', 'OnSite'] }, // Location Type
   textfield_27716477: { type: String, required: true, maxlength: 55}, // Company Name
   website_28133080: String, // Company Url
   textfield_27716361: { type: String, required: true, maxlength: 55 }, // Company Location
-  textarea_27716460: { type: String, required: true, maxlength: 255 }, // company_apply
-
-  created_at: { type: Date, default: Date.now },
-  // updated_at: Date
+  textarea_27716460: { type: String, required: true }, // company_apply
   metadata: {} // Additional information (date_submit, user_agent)
 });
 
